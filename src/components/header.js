5e47439ff88bae68a8a2b9501774fb99a6870a76
Keegan Justis <keeganjustis@Keegans-MacBook-Pro.local>
2020-01-18 14:19:00 -0800

    inital commit

--- src/components/header.js
+++ src/components/header.js
@@ -5,18 +5,41 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#3480eb`,
       marginBottom: `1.45rem`,
     }}
   >
-    <div
+  
+      <div
       style={{
         margin: `0 auto`,
         maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
-      }}
+      }} 
     >
-      <h1 style={{ margin: 0 }}>
+    <nav style={{
+      border-bottom: `1px solid rgba(255,62,0,0.1)`,
+		font-weight: `300`,
+		padding: `0 1em`,
+		color: `#d3dff0`,
+    }}>
+    <ul style={{
+      margin: 0,
+		padding: 0
+    }}>
+		<li><a  href='.'>Home</a></li>
+		<li><a  href='about'>About</a></li>
+		<li><a  href='skills'>Skills</a></li>
+		<li><a  href='social'>Social Media</a></li>
+		 <li><a href='stuff'>Classy Stuff</a></li> 
+		<li><a  href='life'>Visualized Life</a></li>
+		<li><a  href='Contact'>Contact</a></li>
+		
+
+	</ul>
+</nav>
+
+      {/* <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
@@ -26,8 +49,8 @@ const Header = ({ siteTitle }) => (
         >
           {siteTitle}
         </Link>
-      </h1>
-    </div>
+      </h1> */}
+    </div> 
   </header>
 )
 
